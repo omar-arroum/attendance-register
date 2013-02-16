bfc1faefec191bfba6075f823e1300f4c94a4c8a
Omar Arroum <omar.arroum@gmail.com>
2013-02-16 18:16:10 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Attendance::Application.config.secret_token = '91fd78b959ef7c75c139b591e2d5a0c81630d6a9ea7d05b27f6686e764b15d6ada2eeb2f34238d0f946d707e2244178def4a8f3380a6db08863f76ae94145387'
